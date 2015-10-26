d092cd64d8d381d7deb62fee4ae702ca745a20d8
Nathik Salam <nsalam@csc127x21.ad.calpoly.edu>
2015-10-26 11:00:51 -0700

    sample mkl added

---
+++ Proj3/mean_example.c
@@ -0,0 +1,32 @@
+#include <mkl.h>
+#define DIM 1 /* Task dimension */
+#define N 1000 /* Number of observations */
+int main()
+{
+    VSLSSTaskPtr task; /* SS task descriptor */
+
+    double x[N]; /* Array for dataset */
+    double mean; /* Array for mean estimate */ 
+    double* w = 0; /* Null pointer to array of weights, default weight equal to one will be used in the computation */ 
+    MKL_INT p, n, xstorage;
+    int status;
+
+    /* Initialize variables used in the computation of mean */
+    p = DIM;
+    n = N;
+    xstorage = VSL_SS_MATRIX_STORAGE_ROWS;
+    mean = 0.0;
+    
+    /* Step 1 - Create task */
+    status = vsldSSNewTask( &task, &p, &n, &xstorage, x, w, 0 );
+
+    /* Step 2- Initialize task parameters */
+    status = vsldSSEditTask( task, VSL_SS_ED_MEAN, &mean );
+
+    /* Step 3 - Compute the mean estimate using SS one-pass method */
+    status = vsldSSCompute(task, VSL_SS_MEAN, VSL_SS_METHOD_1PASS );
+
+    /* Step 4 - deallocate task resources */
+    status = vslSSDeleteTask( &task );
+    return 0;
+}
