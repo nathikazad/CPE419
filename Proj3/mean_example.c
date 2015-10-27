d7f6a4e42ff27975e17e7f3418c1ee7d76f0683f
Nathik Salam <nsalam@csc127x08.ad.calpoly.edu>
2015-10-26 23:12:18 -0700

    mkl not compiling run icc -mmic -vec -report=3 -O3 mean_example.c -o helloflops1

--- Proj3/mean_example.c
+++ Proj3/mean_example.c
@@ -1,4 +1,10 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
 #include <mkl.h>
+#include <mkl_vsl.h>
 #define DIM 1 /* Task dimension */
 #define N 1000 /* Number of observations */
 int main()
