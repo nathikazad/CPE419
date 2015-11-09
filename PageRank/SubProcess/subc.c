eb74e1e5561ab005c8c1443c045c033c73f36a7b
dazete92 <edazet@calpoly.edu>
2015-11-09 13:57:13 -0800

    updated

--- PageRank/SubProcess/subc.c
+++ PageRank/SubProcess/subc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main (int argc, char ** argv) {
 	int d,e,f,g;
-	scanf("%d %d\n",&d);
-	scanf("%d %d\n",&e);
-	printf("%d",d+e);
+	scanf("%d %d\n",&d, &e);
+	scanf("%d %d\n",&f, &g);
+	printf("%d",d+f);
 }
---
+++ PageRank/subc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char ** argv) {
+	int d,e,f,g;
+	scanf("%d %d\n",&d, &e);
+	scanf("%d %d\n",&f, &g);
+	printf("%d ",d+f);
+   printf("%s ", argv[1]);
+}
